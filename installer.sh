3e0caf6e4a9985880216c0eeb579e9794a9e825e
dveltool <yasriady@yahoo.com>
2016-01-19 12:27:04 +0700

    installer.sh revised

--- installer.sh
+++ installer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 FLENAME=~/dveltool_installer.sh
-rm $FLENAME
+
+if [ -f "$FLENAME" ]; then
+	rm $FLENAME
+fi
+
 wget -O $FLENAME https://raw.githubusercontent.com/dveltool/toolchain/master/installer/dveltool_installer.sh
 chmod +x $FLENAME
 sh $FLENAME
