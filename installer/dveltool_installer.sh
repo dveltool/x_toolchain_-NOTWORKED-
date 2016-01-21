c999dbcba8f9439fedfb5ac4f7c333320b7d044b
dveltool <mfa.daffa@gmail.com>
2016-01-21 16:38:21 +0700

    Update dveltool_installer.sh

--- installer/dveltool_installer.sh
+++ installer/dveltool_installer.sh
@@ -3,11 +3,7 @@
 MYDIR=/tmp
 FLENAME=installer.sh
 
-if [ -f "$MYDIR/$FLENAME" ]; then
-	rm $FLENAME
-fi
-
-wget -P $MYDIR https://raw.githubusercontent.com/dveltool/toolchain/master/installer/installer.sh
+wget -O $MYDIR/$FLENAME $MYDIR https://raw.githubusercontent.com/dveltool/toolchain/master/installer/installer.sh
 chmod +x $MYDIR/$FLENAME
 sh $MYDIR/$FLENAME
 rm $MYDIR/$FLENAME
