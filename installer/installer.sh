a9b4ee2cc8c18a4afc038522dfcf4d69a06c02cb
dveltool <yasriady@yahoo.com>
2016-01-22 15:14:43 +0700

    Installation script for wxWidgets debug added

--- installer/installer.sh
+++ installer/installer.sh
@@ -35,6 +35,9 @@ mkdir -pv $DVELDIR/toolchain/ide
 # ----------------------------------------------------------------------
 
 sh $DVELDIR/toolchain/installer/wxWidgets/linux_x86-64_shared_release.sh
+
+pause
+
 sh $DVELDIR/toolchain/installer/wxWidgets/linux_x86-64_shared_debug.sh
 
 # 020 - wxFormbuilder
